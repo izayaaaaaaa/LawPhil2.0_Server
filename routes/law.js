12143f6f606ad85cc3f90c54ceaf7b2514527e89
abudi6 <aazzouni@ymail.com>
2023-08-05 18:46:36 +0800

    made routes.js for creating routes

--- routes/law.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addLaw } from '../controllers/law.js';
-
-const router = express.Router();
-
-router.post('/addLaw', addLaw);
-
-export default router;
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import { login as loginHandler } from '../controllers/authe.js';
 import { register as registerHandler } from '../controllers/authe.js';
+import { addLaw } from '../controllers/law.js';
 
 const router = express.Router();
 
 router.post('/login', loginHandler);
 router.post('/register', registerHandler);
+router.post('/addLaw', addLaw);
 
 export default router;
