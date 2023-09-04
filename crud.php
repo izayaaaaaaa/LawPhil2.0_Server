959bf96db462848e34dec77a7d15b3cf8304de95
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-09-05 01:42:53 +0800

    shortened updateLaw (without the debug code)

--- crud.php
+++ crud.php
@@ -51,56 +51,40 @@
           echo json_encode(array("error" => "Error creating law: " . $e->getMessage()));
         }
       } else if ($action === 'updateLaw') { // update a law
-          // Check if the required fields are provided
-          if (isset($requestData['id'], $requestData['title'], $requestData['category'], $requestData['content'])) {
-            // sanitize and validate the input data (you may need more validation)
-            $lawId = intval($requestData['id']);
-            $title = filter_var($requestData['title']);
-            $category = filter_var($requestData['category']);
-            $content = filter_var($requestData['content']);
+          // sanitize and validate the input data 
+          $lawId = intval($requestData['id']);
+          $title = filter_var($requestData['title']);
+          $category = filter_var($requestData['category']);
+          $content = filter_var($requestData['content']);
 
-            try {
-              // update the law in the database
-              $stmt = $pdo->prepare("UPDATE laws SET title = :title, category = :category, content = :content WHERE id = :id");
-              $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-              $stmt->bindParam(':category', $category, PDO::PARAM_STR);
-              $stmt->bindParam(':content', $content, PDO::PARAM_STR);
-              $stmt->bindParam(':id', $lawId, PDO::PARAM_INT);
-              $stmt->execute();
-
-              // check if any rows were affected (successful update)
-              if ($stmt->rowCount() > 0) {
-                  echo json_encode(array("message" => "Law updated successfully."));
-              } else {
-                  echo json_encode(array("error" => "Law with ID $lawId not found or no changes made."));
-              }
-            } catch (PDOException $e) {
-                echo json_encode(array("error" => "Error updating law: " . $e->getMessage()));
-            }
-          } else {
-              echo json_encode(array("error" => "Missing required parameters for updating law."));
-          }
+          // update the law in the database
+          $stmt = $pdo->prepare("UPDATE laws SET title = :title, category = :category, content = :content WHERE id = :id");
+          $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+          $stmt->bindParam(':category', $category, PDO::PARAM_STR);
+          $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+          $stmt->bindParam(':id', $lawId, PDO::PARAM_INT);
+          $stmt->execute();
+          
       } else {
-          echo json_encode(array("error" => "Unsupported action."));
+        echo json_encode(array("error" => "Unsupported action."));
       }
     } else {
-        echo json_encode(array("error" => "Action not specified in JSON data."));
+      echo json_encode(array("error" => "Action not specified in JSON data."));
     }
-} 
-  elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE' && isset($_GET['action']) && $_GET['action'] === 'deleteLaw') {
-      // Delete a law by ID
-      if (isset($_GET['id'])) {
-          $lawId = intval($_GET['id']);
-          try {
-              $stmt = $pdo->prepare("DELETE FROM laws WHERE id = :id");
-              $stmt->bindParam(':id', $lawId, PDO::PARAM_INT);
-              $stmt->execute();
-              echo json_encode(array("message" => "Law deleted successfully."));
-          } catch (PDOException $e) {
-              echo json_encode(array("error" => "Error deleting law: " . $e->getMessage()));
-          }
-      } else {
-          echo json_encode(array("error" => "Missing 'id' parameter for deleting law."));
+  } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE' && isset($_GET['action']) && $_GET['action'] === 'deleteLaw') {
+    // Delete a law by ID
+    if (isset($_GET['id'])) {
+      $lawId = intval($_GET['id']);
+      try {
+        $stmt = $pdo->prepare("DELETE FROM laws WHERE id = :id");
+        $stmt->bindParam(':id', $lawId, PDO::PARAM_INT);
+        $stmt->execute();
+        echo json_encode(array("message" => "Law deleted successfully."));
+      } catch (PDOException $e) {
+        echo json_encode(array("error" => "Error deleting law: " . $e->getMessage()));
       }
+    } else {
+      echo json_encode(array("error" => "Missing 'id' parameter for deleting law."));
+    }
   }
 ?>
