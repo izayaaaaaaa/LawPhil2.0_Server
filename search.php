bd8ee949fd7eacaad272fbd05130a44eadf905bf
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-09-12 22:38:06 +0800

    functioning and integrated search

--- search.php
+++ search.php
@@ -1,55 +1,31 @@
-<!-- possible duplication can arise if i group entries by id on a model that has several law tables_to_search
-        -- use one table for all laws with the category distinction as a column -->
-
 <?php
-// Include the database connection script
-require('db.php');
+  // search.php
+  require('db.php');
 
-// Keyword to search
-$keyword = "sample";
+  // Keyword to search
+  if (isset($_GET['q'])) {
+    $keyword = $_GET['q'];
 
-// Initialize the results array
-$results = array();
+    // Initialize the results array
+    $results = array();
 
-// Tables to search (excluding "users" table)
-$tables_to_search = array("laws", "lawstwo");
+    // The table to search
+    $table = "laws";
 
-foreach ($tables_to_search as $table) {
-    $sql = "SELECT * FROM $table WHERE CONCAT(title, ' ', content) LIKE '%$keyword%'";
+    $sql = "SELECT * FROM $table WHERE CONCAT(title, ' ', content) LIKE :keyword";
+    
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':keyword', '%'.$keyword.'%');
+    $stmt->execute();
 
-    $result = $pdo->query($sql); // Use the PDO connection from db.php
-
-    if ($result->rowCount() > 0) {
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            // Check if the ID is already in the results array
-            $id = $row['id'];
-            $found = false;
-            foreach ($results as &$result_item) {
-                if ($result_item['id'] == $id) {
-                    // ID already in results, add the current column to that row
-                    $result_item['columns'][] = array('title' => $row['title'], 'content' => $row['content']);
-                    $found = true;
-                    break;
-                }
-            }
-            if (!$found) {
-                // ID not found in results, create a new row for it
-                $results[] = array('id' => $id, 'columns' => array(array('title' => $row['title'], 'content' => $row['content'])));
-            }
-        }
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $results[] = $row;
     }
-}
 
-// Close the database connection (if needed)
-$pdo = null;
-
-// Output the results
-foreach ($results as $result_item) {
-    echo "ID: " . $result_item['id'] . "<br>";
-    foreach ($result_item['columns'] as $column) {
-        echo "Title: " . $column['title'] . "<br>";
-        echo "Content: " . $column['content'] . "<br>";
-    }
-    echo "<hr>";
-}
-?>
+    // Return JSON result with a response code
+    http_response_code(200);
+    echo json_encode($results);
+  } else {
+    http_response_code(400);
+    echo json_encode(array('error' => 'Bad Request'));
+  }
