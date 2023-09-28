024a635c7e1e4ff187664fa565d69ffee364cdd3
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-09-28 17:02:59 +0800

    search with filters functional

--- lawCRUD/searchKeywordLaw.php
+++ lawCRUD/searchKeywordLaw.php
@@ -1,22 +1,42 @@
 <?php
-  // search.php
+  // searchKeywordLaw.php
   require '../db.php';
 
   // Keyword to search
-  if (isset($_GET['q'])) {
-    $keyword = $_GET['q'];
+  if (isset($_GET['searchQuery'])) {
+    $keyword = $_GET['searchQuery'];
+    $category = isset($_GET['selectedCategory']) ? $_GET['selectedCategory'] : '';
+    $searchType = isset($_GET['searchType']) ? $_GET['searchType'] : 'both'; // Default search type is 'both'
 
-    // Initialize the results array
     $results = array();
 
     // The table to search
     $table = "laws";
 
-    $sql = "SELECT * FROM $table WHERE CONCAT(title, ' ', content) LIKE :keyword";
+$sql = "SELECT * FROM $table WHERE ";
+
+    if (!empty($category)) {
+      // Add category filtering
+      $sql .= "category = :category AND ";
+    }
+
+    // Update the SQL query to consider search type
+    if ($searchType === 'title') {
+            $sql .= "title LIKE :keyword";
+        } elseif ($searchType === 'content') {
+            $sql .= "content LIKE :keyword";
+        } else {
+      $sql .= "CONCAT(title, ' ', content) LIKE :keyword";
+    }
     
     $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':keyword', '%'.$keyword.'%');
-    $stmt->execute();
+
+    if (!empty($category)) {
+      $stmt->bindValue(':category', $category);
+    }
+    $stmt->bindValue(':keyword', '%' . $keyword . '%');
+
+        $stmt->execute();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $results[] = $row;
@@ -29,3 +49,4 @@
     http_response_code(400);
     echo json_encode(array('error' => 'Bad Request'));
   }
+?>
