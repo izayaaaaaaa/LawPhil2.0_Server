1d30069e49dc94926c0d1eea624182b5d54ace40
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-10-02 22:52:05 +0800

    law content pages functional

--- lawCRUD/getSpecificLaw.php
+++ lawCRUD/getSpecificLaw.php
@@ -1,11 +1,11 @@
 <?php
   require '../db.php';
 
-  $id = $_GET['id'];
+  $lawID = $_GET['lawID'];
   $query = 'SELECT title, category, content FROM laws WHERE id = ?';
   
   $stmt = $pdo->prepare($query);
-  $stmt->bindParam(1, $id);
+  $stmt->bindParam(1, $lawID);
   $stmt->execute();
   $row = $stmt->fetch(PDO::FETCH_ASSOC);
   echo json_encode($row);
